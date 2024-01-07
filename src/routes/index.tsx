674479204078d31b07af2b139da7c9ebf59fb5c4
carla-reis-cr <carla.cr.reis@gmail.com>
2024-01-07 17:34:59 -0300

    feat: configuração de tema

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,14 @@ import { Button } from '@mui/material'
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Button>Teste</Button>} />
+      <Route
+        path="/home"
+        element={
+          <Button variant="contained" color="primary">
+            Teste
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/Home" />} />
     </Routes>
   )
