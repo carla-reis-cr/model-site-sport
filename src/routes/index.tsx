77a588c85f1d8e7783887ea907b7ca6a902f6ff4
carla-reis-cr <carla.cr.reis@gmail.com>
2024-01-07 16:47:35 -0300

    feat: adicionado react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {}
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<p>Página Inicial</p>} />
+      <Route path="*" element={<Navigate to="/Home" />} />
+    </Routes>
+  )
+}
